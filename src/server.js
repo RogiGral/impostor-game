5e59c813e90ff9cc86728a046ac59002a9164c3e
RogiGral <igor.gralak99@gmail.com>
2025-10-13 21:13:36 +0200

    socket io skeleton

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 // Serve static files if needed
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
