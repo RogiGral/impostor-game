87adb3db103bad392c1a5ad4dbc43f8efcd6d14c
RogiGral <igor.gralak99@gmail.com>
2025-10-14 22:30:44 +0200

    Fix static file serving and add route for client HTML

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/client.html"));
+});
 
 function generateRoomCode() {
   return Math.floor(1000 + Math.random() * 9000).toString();
